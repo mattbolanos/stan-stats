be901b0c392322f4ec6a3af7c5f46038fec1c6ed
mattbolanos <matthew.a.bolanos@gmail.com>
2024-07-14 23:35:17 -0400

    add charts page

--- components/nav-bar.tsx
+++ components/nav-bar.tsx
@@ -7,19 +7,29 @@ import {
   NavigationMenuLink,
   NavigationMenuList,
   NavigationMenuTrigger,
+  navigationMenuTriggerStyle,
 } from "@/components/ui/navigation-menu";
+import Image from "next/image";
+import Link from "next/link";
 
 export default function NavBar() {
   return (
-    <NavigationMenu>
-      <NavigationMenuList>
-        <NavigationMenuItem>
-          <NavigationMenuTrigger>Item One</NavigationMenuTrigger>
-          <NavigationMenuContent>
-            <NavigationMenuLink>Link</NavigationMenuLink>
-          </NavigationMenuContent>
-        </NavigationMenuItem>
-      </NavigationMenuList>
-    </NavigationMenu>
+    <nav className="flex items-center justify-between p-4">
+      <div className="flex items-center max-w-screen-2xl">
+        <div className="flex items-center space-x-2 nav-logo">
+          <Image src="/spotify-color.svg" alt="logo" width={24} height={24} />
+          <span className="font-bold">SonoGraph</span>
+        </div>
+        <nav className="flex items-center gap-4 text-sm lg:gap-6">
+          <Link href="/" legacyBehavior passHref>
+            Explore
+          </Link>
+
+          <Link href="/charts" legacyBehavior passHref>
+            Charts
+          </Link>
+        </nav>
+      </div>
+    </nav>
   );
 }
