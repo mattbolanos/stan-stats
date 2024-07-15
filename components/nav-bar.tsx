0793d875f269ce1dfbcf2ee2459474dd8fe0600a
mattbolanos <matthew.a.bolanos@gmail.com>
2024-07-15 00:32:01 -0400

    nice

--- components/nav-bar.tsx
+++ components/nav-bar.tsx
@@ -1,20 +1,17 @@
 "use client";
 
-import {
-  NavigationMenu,
-  NavigationMenuContent,
-  NavigationMenuItem,
-  NavigationMenuLink,
-  NavigationMenuList,
-  NavigationMenuTrigger,
-  navigationMenuTriggerStyle,
-} from "@/components/ui/navigation-menu";
 import Image from "next/image";
 import Link from "next/link";
+import {
+  GitHubLogoIcon,
+  HeartFilledIcon,
+  InfoCircledIcon,
+} from "@radix-ui/react-icons";
+import { ThemeToggle } from "./theme-provider";
 
 export default function NavBar() {
   return (
-    <nav className="flex items-center justify-between p-4">
+    <nav className="flex items-center justify-between px-8 py-4">
       <div className="flex items-center max-w-screen-2xl">
         <div className="flex items-center space-x-2 nav-logo">
           <Image src="/spotify-color.svg" alt="logo" width={24} height={24} />
@@ -24,11 +21,16 @@ export default function NavBar() {
           <Link href="/" legacyBehavior passHref>
             Explore
           </Link>
-
           <Link href="/charts" legacyBehavior passHref>
             Charts
           </Link>
         </nav>
+      </div>
+      <div className="flex justify-end gap-4 items-center">
+        <InfoCircledIcon height={24} width={24} />
+        <GitHubLogoIcon height={24} width={24} />
+        <HeartFilledIcon height={24} width={24} color="red" />
+        <ThemeToggle />
       </div>
     </nav>
   );
---
+++ components/theme-provider.tsx
@@ -0,0 +1,27 @@
+"use client";
+
+import * as React from "react";
+import { ThemeProvider as NextThemesProvider, useTheme } from "next-themes";
+import { type ThemeProviderProps } from "next-themes/dist/types";
+import { MoonIcon, SunIcon } from "@radix-ui/react-icons";
+import { Button } from "@/components/ui/button";
+
+export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
+  return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
+}
+
+export function ThemeToggle() {
+  const { setTheme, resolvedTheme } = useTheme();
+
+  return (
+    <Button
+      variant="ghost"
+      size="icon"
+      onClick={() => setTheme(resolvedTheme === "dark" ? "light" : "dark")}
+    >
+      <SunIcon className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+      <MoonIcon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+      <span className="sr-only">Toggle theme</span>
+    </Button>
+  );
+}
