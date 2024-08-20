f86cf7cdc7a27e1f061750f098ad5da429fbb7c0
mattbolanos <matthew.a.bolanos@gmail.com>
2024-08-21 01:24:16 +0200

    color stuff

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,12 @@
 import { DisplayCards } from "@/components/display-cards";
 import ExploreCardsParent from "@/components/explore-cards-parent";
 import { ExploreChart } from "@/components/explore-chart";
+import {
+  Card,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 import { supabase } from "@/lib/supabase";
 import { ArtistDetailsResponse, ArtistSample } from "@/lib/types";
 import {
@@ -11,7 +17,7 @@ import {
   queryArtistDetails,
 } from "@/lib/utils";
 import { ClockIcon } from "@radix-ui/react-icons";
-import { Disc3, Mic2Icon, Music2Icon } from "lucide-react";
+import { DatabaseIcon, Disc3Icon, Mic2Icon, Music2Icon } from "lucide-react";
 
 async function getDefaultArtistSample(
   size: number = DEFAULT_ARTIST_SAMPLE_SIZE
@@ -73,7 +79,11 @@ async function getDefaultDetails(
   return details;
 }
 
-async function getTotals() {
+async function getTotals(): Promise<{
+  totalArtists: number;
+  totalAlbums: number;
+  totalSingles: number;
+}> {
   "use server";
 
   const [artistCount, albumTotal, singleTotal] = await Promise.all([
@@ -81,7 +91,6 @@ async function getTotals() {
       .from("spotify_artists_meta")
       .select("id", { count: "exact", head: true })
       .then(({ count }) => count),
-
     supabase
       .from("spotify_artists_meta")
       .select("album_total:albums_count.sum()")
@@ -98,9 +107,9 @@ async function getTotals() {
   ]);
 
   return {
-    totalArtists: artistCount,
-    totalAlbums: albumTotal,
-    totalSingles: singleTotal,
+    totalArtists: artistCount ?? 0,
+    totalAlbums: albumTotal ?? 0,
+    totalSingles: singleTotal ?? 0,
   };
 }
 
@@ -111,6 +120,88 @@ async function getDisplayDetails(): Promise<ArtistDetailsResponse> {
   return details;
 }
 
+const HeroCard = ({
+  icon: Icon,
+  title,
+  description,
+}: {
+  icon: React.ElementType;
+  title: string;
+  description: string;
+}) => {
+  return (
+    <Card className="w-full sm:w-[calc(50%-0.5rem)] p-2 min-h-[190px] bg-primary-foreground">
+      <CardHeader>
+        <CardTitle className="flex items-center gap-2">
+          <div className="p-2 rounded-md bg-secondary">
+            <Icon className="w-6 h-6 shrink-0 opacity-75 text-green-500" />
+          </div>
+          <p className="text-base font-bold">{title}</p>
+        </CardTitle>
+      </CardHeader>
+      <CardDescription className="px-5 py-3 pb-1 text-muted-foreground">
+        {description}
+      </CardDescription>
+    </Card>
+  );
+};
+
+const HeroCards = ({
+  dateRange,
+  totalArtists,
+  totalAlbums,
+  totalSingles,
+}: {
+  dateRange: {
+    min: string;
+    max: string;
+  };
+  totalArtists: number;
+  totalAlbums: number;
+  totalSingles: number;
+}) => {
+  const cardData = [
+    {
+      icon: ClockIcon,
+      title: `Since ${new Date(dateRange.min).toLocaleString("default", {
+        month: "long",
+        year: "numeric",
+        timeZone: "UTC",
+      })}`,
+      description:
+        "Spotify updates monthly listener data daily. This figure represents the average number of unique listeners over the past 28 days who have played an artist's music at least once.",
+    },
+    {
+      icon: Mic2Icon,
+      title: `${formatMonthlyListeners(totalArtists ?? 0)} Artists`,
+      description:
+        "We use Spotify's API Search endpoint with a myriad of queries to find a wide breadth of artists. An artist must have 5k+ followers to be included in our database.",
+    },
+    {
+      icon: Disc3Icon,
+      title: `${formatMonthlyListeners(totalAlbums ?? 0)} Abums`,
+      description: "The total number of albums in the dataset.",
+    },
+    {
+      icon: Music2Icon,
+      title: `${formatMonthlyListeners(totalSingles ?? 0)} singles`,
+      description: "The total number of singles in the dataset.",
+    },
+  ];
+
+  return (
+    <div className="flex flex-wrap gap-4 max-w-4xl mx-auto">
+      {cardData.map((card, index) => (
+        <HeroCard
+          key={index}
+          icon={card.icon}
+          title={card.title}
+          description={card.description}
+        />
+      ))}
+    </div>
+  );
+};
 export default async function Home() {
   const defaultArtistSample = await getDefaultArtistSample();
   const dateRange = await getDateRange();
@@ -128,37 +219,26 @@ export default async function Home() {
             Numbers
           </h1>
           <p
-            className="text-xl text-muted-foreground max-w-xl mt-5 text-center"
+            className="text-xl text-muted-foreground max-w-3xl mt-5 text-center"
             style={{ textWrap: "balance" }}
           >
             Stan like a pro. Compare your faves. See who&apos;s hot and
-            who&apos;s not.
-          </p>
-        </div>
-        <div className="flex flex-col gap-3 items-center w-full">
-          <p className="text-md flex items-center gap-1.5">
-            <ClockIcon className="w-5 h-5 opacity-75 shrink-0" />
-            Since{" "}
-            {new Date(dateRange.min).toLocaleString("default", {
-              month: "long",
-              year: "numeric",
-              timeZone: "UTC",
-            })}
-          </p>
-          <p className="text-md flex items-center gap-1.5">
-            <Mic2Icon className="w-5 h-5 opacity-75 shrink-0" />
-            {formatMonthlyListeners(totalArtists ?? 0)} artists
-          </p>
-          <p className="text-md flex items-center gap-1.5">
-            <Disc3 className="w-5 h-5 opacity-75 shrink-0" />
-            {formatMonthlyListeners(totalAlbums ?? 0)} albums
-          </p>
-          <p className="text-md flex items-center gap-1.5">
-            <Music2Icon className="w-5 h-5 opacity-75 shrink-0" />
-            {formatMonthlyListeners(totalSingles ?? 0)} singles
+            who&apos;s not. Free and open source.
           </p>
         </div>
       </div>
+      <div className="flex justify-center items-center w-full">
+        <div className="w-fit flex items-center gap-2 border px-4 py-3 rounded-lg border-yellow-500 bg-primary-foreground">
+          <DatabaseIcon className="w-5 h-5" />
+          <p className="text-md font-bold">About the Database</p>
+        </div>
+      </div>
+      <HeroCards
+        dateRange={dateRange}
+        totalArtists={totalArtists}
+        totalAlbums={totalAlbums}
+        totalSingles={totalSingles}
+      />
       <div className="flex flex-col gap-5 opacity-85 items-center">
         <DisplayCards artists={displayDetails.meta} />
       </div>
