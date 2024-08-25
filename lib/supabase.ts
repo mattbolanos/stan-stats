d00fb9fb8430caaf9059289c5373d28ead1ea721
mattbolanos <matthew.a.bolanos@gmail.com>
2024-08-25 18:41:22 -0400

    add revalidate

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,12 +1,18 @@
 // lib/supabase.js
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseURL = process.env.SUPABASE_URL || "http://localhost:3000";
-const supabaseServiceKey = process.env.SUPABASE_SERVICE_KEY || "test";
+const supabaseURL = process.env.NEXT_SUPABASE_URL || "http://localhost:3000";
+const supabaseServiceKey = process.env.NEXT_SUPABASE_SERVICE_KEY || "test";
 
 export const supabase = createClient(supabaseURL, supabaseServiceKey, {
   auth: {
     autoRefreshToken: false,
     persistSession: false,
+  },
+  global: {
+    fetch: (...args) => {
+      const [url, options] = args;
+      return fetch(url, { ...options, next: { revalidate: 57600 } });
+    },
   },
 });
